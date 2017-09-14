7cb68325a639e75c51f694baa76239723a9bbc55
Will Smith <willsmithte@gmail.com>
2017-09-14 10:49:32 +1000

    Convert Person from interface to superclass

--- src/Player.java
+++ src/Player.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by Elber on 05-Aug-17.
  */
-public class Player implements Person{
+public class Player extends Person{
     private String name;
     private int chips;
+    private int bet;
     public Player(String name){
         this.name = name;
         chips = 500;
     }
+    @Override
     public void haveTurn(){
-
+        System.out.println(name + "'s turn: "+ hand + " - Current score = "+getScore());
     }
     public int getChips(){
         return chips;
@@ -22,5 +25,10 @@ public class Player implements Person{
     @Override
     public String toString(){
         return name + ": " + chips + " chips.";
+    }
+
+    public void placeBet() {
+        System.out.print(this + " How many chips would you like to bet? ");
+        bet = new Scanner(System.in).nextInt();
     }
 }
